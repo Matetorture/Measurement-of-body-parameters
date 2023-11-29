19a72ebd7f3589232db7a8a6d8ca0cd929639e56
Matetorture <przemek.pytlarz18@gmail.com>
2023-11-29 02:15:13 +0100

    add search input name in test

---
+++ wwwroot/js/search.js
@@ -0,0 +1,50 @@
+const OpenSearch = document.querySelector("#OpenSearch");
+const SearchDiv = document.querySelector("#SearchDiv");
+const SearchInput = SearchDiv.querySelector("input");
+const SearchName = document.querySelectorAll(".SearchNameDisplay");
+var isOpenSearch = false;
+
+OpenSearch.addEventListener("click", () => {
+    if (isOpenSearch) {
+        SearchDiv.style.display = "none";
+    } else {
+        SearchDiv.style.display = "block";
+    }
+    isOpenSearch = !isOpenSearch;
+
+    SearchInput.focus();
+});
+
+
+document.addEventListener("click", (event) => {
+    if (event.target !== OpenSearch && event.target !== SearchDiv && event.target !== SearchInput && event.target !== OpenSearch.querySelector("span")) {
+        SearchDiv.style.display = "none";
+        isOpenSearch = false;
+    }
+});
+
+SearchInput.addEventListener("input", () => {
+    SearchOnly(SearchInput.value);
+});
+
+const SearchOnly = (search) => {
+    SearchName.forEach((e, i) => {
+
+        let mainString = e.getAttribute('string');
+        e.innerHTML = mainString;
+
+        if (mainString.toLowerCase().includes(search.toLowerCase())) {
+            e.parentElement.style.display = "table-row";
+
+            let startIndex = mainString.indexOf(search);
+            let extractedText = mainString.substring(startIndex, startIndex + search.length);
+            let textBefore = mainString.substring(0, startIndex);
+            let textAfter = mainString.substring(startIndex + search.length);
+
+            let newText = textBefore + `<span class="bg-warning">` + extractedText + `</span>` + textAfter;
+            e.innerHTML = newText;
+        } else {
+            e.parentElement.style.display = "none";
+        }
+    });
+}
